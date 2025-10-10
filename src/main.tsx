6d2068f3ab38a734e208d327985706790734a8fb
AbrahamMougle <abrahammougle76@gmail.com>
2025-10-10 18:04:58 +0200

    suite a une erreur au niveau du vercel je viens supprimer les extensions tsx et ts car je l'ai ai ajouter  quand je l'ai importer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { makeServer } from './Api/server.ts'
+import App from './App'
+import { makeServer } from './Api/server'
 import { Toaster } from "@/components/ui/sonner"
 
 
