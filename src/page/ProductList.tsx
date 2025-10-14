d7a06802c4fd6f63c9ab96c69bbb98197255e78b
AbrahamMougle <abrahammougle76@gmail.com>
2025-10-14 20:19:25 +0200

    suppression des pages

--- src/page/ProductList.tsx
+++ src/page/ProductList.tsx
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
-import { useStore } from "../store/store";
+import { useEffect, useState } from "react";
 import { ProductCard } from "@/composant/ProductCard";
+interface ProductType{
+  id:number,
+  name:string
+  price:number
+  image:string
+  imageSmilaire:Array<string>
+  description:'string'
+  category:'Ordinateurs'|'Claviers'|'Casques'|'Casques'|'Bluetooth'
+}
 
 export default function ProductListPage() {
-  const { products, setProducts } = useStore();
-
+  const [pro,setPro]=useState<ProductType[]>()
   useEffect(() => {
     fetch("/api/products")
       .then((res) => res.json())
-      .then((data) => setProducts(data.products))
+      .then((data) => setPro(data.products))
       .catch((err) => console.error("Erreur de chargement :", err));
   }, []);
-  
+  if (!pro) {
+    return <div>loading</div>
+    
+  }
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-50 to-slate-100 py-10 px-8">
@@ -20,7 +30,7 @@ export default function ProductListPage() {
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-8">
-        {products.map((product) => (<ProductCard id={product.id} key={product.id} name={product.name} image={product.image} price={product.price} />
+        {pro.map((product) => (<ProductCard id={product.id} key={product.id} name={product.name} image={product.image} price={product.price} />
           
         ))}
       </div>
