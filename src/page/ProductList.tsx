efc55f0545ea78fc5f8e96e05556c1f4b84cbc5f
AbrahamMougle <abrahammougle76@gmail.com>
2025-10-14 21:12:32 +0200

    amelioration de l'interface UI design  en utilisant useLoader pour fetch les donne avant le rendu du composant productListe

--- src/page/ProductList.tsx
+++ src/page/ProductList.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import { ProductCard } from "@/composant/ProductCard";
+import { useLoaderData } from "react-router-dom";
 interface ProductType{
   id:number,
   name:string
@@ -9,20 +9,13 @@ interface ProductType{
   description:'string'
   category:'Ordinateurs'|'Claviers'|'Casques'|'Casques'|'Bluetooth'
 }
-
+export  async function fetchData():Promise<ProductType[]> {
+   const res= await fetch("/api/products")
+   const dataJson= await res.json()
+  return dataJson.products 
+}
 export default function ProductListPage() {
-  const [pro,setPro]=useState<ProductType[]>()
-  useEffect(() => {
-    fetch("/api/products")
-      .then((res) => res.json())
-      .then((data) => setPro(data.products))
-      .catch((err) => console.error("Erreur de chargement :", err));
-  }, []);
-  if (!pro) {
-    return <div>loading</div>
-    
-  }
-
+  const dataProduct=useLoaderData<typeof fetchData>()
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-50 to-slate-100 py-10 px-8">
       <h1 className="text-3xl font-bold text-slate-800 mb-8 text-center tracking-tight">
@@ -30,7 +23,7 @@ export default function ProductListPage() {
       </h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-8">
-        {pro.map((product) => (<ProductCard id={product.id} key={product.id} name={product.name} image={product.image} price={product.price} />
+        {dataProduct.map((product) => (<ProductCard id={product.id} key={product.id} name={product.name} image={product.image} price={product.price} />
           
         ))}
       </div>
