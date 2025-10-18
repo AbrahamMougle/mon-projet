e0ceb8247bac4d8929eff8883b020e79daa691ed
AbrahamMougle <abrahammougle76@gmail.com>
2025-10-18 07:11:51 +0200

    Ajout du slice Dark aux stores

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,8 +4,11 @@ import { createCartSlice } from "./cartSlice";
 import { createOrderSlice} from "./OrderSlice";
 import type { OrderSlice } from "./OrderSlice";
 import type { CartSlice } from "./cartSlice";
+import { useThemeStore } from "./DarkSlice";
+import type{ ThemeSliceState } from "./DarkSlice";
+
 import { createUserSlice, type UserSlice } from "./UserSlice";
-type Store = CartSlice&OrderSlice&UserSlice; 
+type Store = CartSlice&OrderSlice&UserSlice&ThemeSliceState; 
 
 export const useStore = create<Store>()(
   devtools(
@@ -13,7 +16,9 @@ export const useStore = create<Store>()(
       (set,get,api) => ({
         ...createOrderSlice(set,get,api),
         ...createCartSlice(set,get,api),
-        ...createUserSlice(set,get,api)
+        ...createUserSlice(set,get,api),
+        ...useThemeStore(set, get, api),
+      
       }),
       { name: "ecommerce-cart" } // clé localStorage
     )
